9473848403ad2c99fa057dceca7542fcee1331bb
ssyygy <ssyygy4@gmail.com>
2026-06-14 22:11:37 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ssyyg_nat7t2w/OneDrive/Desktop/arcade_in_console
-# Build directory: C:/Users/ssyyg_nat7t2w/OneDrive/Desktop/arcade_in_console/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[all_tests]=] "C:/Users/ssyyg_nat7t2w/OneDrive/Desktop/arcade_in_console/build/game_tests.exe")
-set_tests_properties([=[all_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ssyyg_nat7t2w/OneDrive/Desktop/arcade_in_console/CMakeLists.txt;40;add_test;C:/Users/ssyyg_nat7t2w/OneDrive/Desktop/arcade_in_console/CMakeLists.txt;0;")
-subdirs("_deps/doctest-build")
